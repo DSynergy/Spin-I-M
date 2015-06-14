f45ec9662bce8d1cec42a36709e353b9ee640d8f
Edward McCarthy <efmccarthy26@gmail.com>
2015-06-13 18:53:01 -0600

    validated for email format. Annoying. Removed the 'on: :create' nonsense which breaks tests

--- Spin-I-M/app/models/user.rb
+++ Spin-I-M/app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
+  validates :username, presence: true
+  validates :email, presence: true, length: { maximum: 75 }
+
+  VALID_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates_format_of :email, :with => VALID_REGEX
+
   has_many :playlists, through: :user_playlists
 end
--- Spin-I-M/test/models/user_test.rb
+++ Spin-I-M/test/models/user_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.create(username: "beach_bum", email: "sandlover@example.com")
+  end
+
+  test  "should be valid" do
+    assert @user.valid?
+  end
+
+  test "username should be present" do
+    @user.username = "   "
+    refute @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "   "
+    refute @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 100 + "@example.com"
+    refute @user.valid?
+  end
+
+  test "validation only accepts valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "validation rejects invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      refute @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
 end
