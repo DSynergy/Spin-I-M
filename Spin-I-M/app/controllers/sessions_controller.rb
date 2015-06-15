1bc79c64376f57a05f7b9cda7f2a0151be39f5fd
Edward McCarthy <efmccarthy26@gmail.com>
2015-06-15 16:57:07 -0600

    can login with email. Like a boss.

--- Spin-I-M/app/controllers/sessions_controller.rb
+++ Spin-I-M/app/controllers/sessions_controller.rb
@@ -3,7 +3,8 @@ class SessionsController < ApplicationController
   def create
    @user = User.find_by(email: params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
-      flash[:notice] = "Successfully logged in as #{set_name(@user)}"
+    #  binding.pry
+      flash[:notice] = "Successfully logged in #{@user.username}"
       session[:user_id] = @user.id
       redirect_to playlists_path
     else
---
+++ Spin-I-M/test/features/user_login_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class UserLoginTest < ActionDispatch::IntegrationTest
+  include Capybara::DSL
+
+  def setup
+    user = User.create(username: "george",
+                      email: "george.walker@bush.com",
+                      password: "password",
+                      password_confirmation: "password")
+  end
+
+  test "user can log in" do
+    visit root_path
+
+    click_link_or_button "Log In"
+    assert_equal login_path, current_path
+    # save_and_open_page
+    fill_in "Email", with: "george.walker@bush.com"
+    fill_in "Password", with: "password"
+
+    click_link_or_button "Login To Spin-I-M"
+    save_and_open_page
+    assert_equal playlists_path, current_path
+  end
+
+
+end
