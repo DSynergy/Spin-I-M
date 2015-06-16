b0ae34efa2df471fb3a50ecffd5f5eb346901fa7
Rich Shea <richcshea@gmail.com>
2015-06-16 14:36:06 -0600

    updated seed data

--- Spin-I-M/db/seeds.rb
+++ Spin-I-M/db/seeds.rb
@@ -3,5 +3,9 @@ User.create(username: "user",
                        password: "password",
                        password_confirmation: "password")
 
+playlist = Playlist.create(name: "Hip Hop")
+playlist.songs.create(artist: "Michael Jackson", title: "Thriller")
+playlist.songs.create(artist: "Billy Joel", title: "Piano Man")
+playlist.songs.create(artist: "Taylor Swift", title: "Shake it off")
 
-Playlist.create(name: "Hip Hop")
+
