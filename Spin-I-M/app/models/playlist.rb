3fdadf2c8ff24a5908c951feab6430258aa6bc7a
Dustin Gault <Dustin.Gault@gmail.com>
2015-06-20 14:59:55 -0600

    styling and further api work

--- Spin-I-M/app/models/playlist.rb
+++ Spin-I-M/app/models/playlist.rb
@@ -1,6 +1,31 @@
 class Playlist < ActiveRecord::Base
   has_many :playlist_songs
   has_many :songs, through: :playlist_songs
-
   has_many :users, through: :user_playlists
+
+  attr_reader :queue
+
+  def create_queue
+    @queue = []
+    self.songs.each do |song|
+      queue << song.url
+    end
+    queue
+  end
+
+  def currently_playing
+    create_queue
+    current_song = queue.first
+    queue.shift
+    current_song
+  end
+
+  def shuffle_playlist
+    queue.shuffle
+  end
+
+  def song_on_deck
+    queue.second
+  end
+
 end
