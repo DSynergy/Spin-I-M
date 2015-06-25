edfd0a1704c1380a68142d0a7ace51ecb3eeacb6
Dustin Gault <Dustin.Gault@gmail.com>
2015-06-25 09:57:24 -0600

    added playlist images

--- Spin-I-M/db/schema.rb
+++ Spin-I-M/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150616201124) do
+ActiveRecord::Schema.define(version: 20150625154825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20150616201124) do
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
     t.string   "description"
+    t.string   "image_url"
   end
 
   create_table "songs", force: :cascade do |t|
--- Spin-I-M/db/seeds.rb
+++ Spin-I-M/db/seeds.rb
@@ -3,26 +3,26 @@ User.create(username: "user",
                        password: "password",
                        password_confirmation: "password")
 
-playlist = Playlist.create(name: "Hip Hop")
+playlist = Playlist.create(name: "Hip Hop", image_url: "http://cdn.pitchfork.com/albums/9474/homepage_large.02cbc9b1.jpg")
 
 playlist.songs.create(artist: "Snoop Dogg", title: "Serial Killer", url: "https://soundcloud.com/user714971037/sets/snoop-dogg-serial-killa")
 playlist.songs.create(artist: "Damian Marley", title: "Cypress Hill & Rusko feat Damian Marley - Can't Keep Me Down", url: "https://soundcloud.com/damianmarley/cypress-hill-rusko-feat-damian")
 playlist.songs.create(artist: "Pretty Lights", title: "Jay Z Empire State Of Mind Remix", url: "https://soundcloud.com/prettylights/pretty-lights-jay-z-empire")
 
-playlist2 = Playlist.create(name: "House")
+playlist2 = Playlist.create(name: "House", image_url: "http://djautograph.com/wp-content/uploads/2013/10/House_Music_by_Labelrx.png")
 
 playlist2.songs.create(artist: "David August", title: "Epikur", url: "https://soundcloud.com/frankyboy-1/david-august-epikur")
 playlist2.songs.create(artist: "Bonobo", title: "First Fires - Maya Jane Coles Remix", url: "https://soundcloud.com/mayajanecoles/bonobo-first-fires-feat-grey-1")
 playlist2.songs.create(artist: "Moderat", title: "A New Error", url: "https://soundcloud.com/bpitch-control/moderat-a-new-error")
 playlist2.songs.create(artist: "Avatism", title: "Mastadon - Mind Against Remix", url: "https://soundcloud.com/technochickens/avatism-mastodon-mind-against")
 
-playlist3 = Playlist.create(name: "Mac's Selections")
+playlist3 = Playlist.create(name: "Mac's Selections", image_url: "https://avatars2.githubusercontent.com/u/8164007?v=3&s=400")
 
 playlist3.songs.create(artist: "Ratatat", title: "Seventeen Years", url: "https://soundcloud.com/rochielle-parkes/ratatat-seventeen-years")
 playlist3.songs.create(artist: "Black Keys", title: "Little Black Submarines", url: "https://soundcloud.com/matrem/little-black-submarines-the")
 playlist3.songs.create(artist: "The Heavy", title: "Colleen", url: "https://soundcloud.com/swag-the-blog/the-heavy-coleen-feat-the-dap")
 
-playlist4 = Playlist.create(name: "Dustin's Selections")
+playlist4 = Playlist.create(name: "Dustin's Selections", image_url: "https://avatars2.githubusercontent.com/u/7361182?v=3&s=460")
 
 playlist4.songs.create(artist: "Eli and Fur", title: "You're So High", url: "https://soundcloud.com/eliandfur/youre-so-high")
 playlist4.songs.create(artist: "Mt. Eden", title: "Still Alive", url: "https://soundcloud.com/mtedenofficial/mt-eden-still-alive")
@@ -30,7 +30,7 @@ playlist4.songs.create(artist: "De-Phazz", title: "Cut The Jazz", url: "https://
 playlist4.songs.create(artist: "Drop Frame", title: "Catalyst EP - 04 Swim Feat. Annie Inkerman - Phetsta Collab", url: "https://soundcloud.com/dropframe/big-drop-trance-track")
 playlist4.songs.create(artist: "Major Lazer and DJ Snake", title: "Lean On", url: "https://soundcloud.com/majorlazer/major-lazer-dj-snake-lean-on-feat-mo")
 
-playlist5 = Playlist.create(name: "Rock")
+playlist5 = Playlist.create(name: "Rock", image_url: "https://upload.wikimedia.org/wikipedia/commons/b/b4/Logan_Rock_Treen_closeup.jpg")
 
 playlist5.songs.create(artist: "Led Zepplin", title: "Kashmir", url: "https://soundcloud.com/kuploadr2/led-zeppelin-kashmir")
 playlist5.songs.create(artist: "Queens of the Stone Age", title: "Mosquito Song", url: "https://soundcloud.com/postal-lady/queens-of-the-stone-age-mosquito-song")
