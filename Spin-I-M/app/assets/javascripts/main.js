19e1bbd9f38d4bd811fc47fa11692a98fd4f7ac7
Edward McCarthy <efmccarthy26@gmail.com>
2015-07-08 19:54:53 -0600

    A User prefix can now actually add messages

--- Spin-I-M/app/assets/javascripts/main.js
+++ Spin-I-M/app/assets/javascripts/main.js
@@ -1,6 +1,7 @@
 const username = (document.cookie).replace("username=", "");
 
 $(document).ready(function () {
+
   var socket = io.connect("http://localhost:3001");
 
   $('#be_heard').click(function () {
@@ -12,17 +13,18 @@ $(document).ready(function () {
   // socket.on('message', function(message){
   //   $('#messages').append("<li style='list-style-type: none;'>" + message.body + "</li>");
   // });
+
   socket.on('message', function (message) {
 		$('#messages').append("<li style='list-style-type: none;'>" +
                           '<b>' + username + ':</b>' +
                           message.body + "</li>" + '<br>');
 	});
 
-  socket.on('updateusers', function(data) {
-		$('#users').empty();
-		$.each(data, function(key, value) {
-			$('#users').append('<div>' + key + '</div>');
-		});
-	});
+  // socket.on('updateusers', function(data) {
+	// 	$('#users').empty();
+	// 	$.each(data, function(key, value) {
+	// 		$('#users').append('<div>' + key + '</div>');
+	// 	});
+	// });
 
 });
